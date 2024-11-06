6fcb91dfffe9f675d9cae12fd27f2e7d7cb99dfb
Gaurav Shetty <45145698+iamgsshetty@users.noreply.github.com>
2024-11-06 16:50:01 +0100

    Update createSACAgent.m
    added comments

--- createSACAgent.m
+++ createSACAgent.m
@@ -1,41 +1,70 @@
 function agent = createSACAgent(numObs, obsInfo, numAct, actInfo, Ts)
-% Walking Robot -- DDPG Agent Setup Script
-% Copyright 2024 The MathWorks, Inc.
+    % createSACAgent Creates a Soft Actor-Critic (SAC) agent.
+    %
+    % This function initializes an SAC agent for reinforcement learning by
+    % creating the actor and critic networks, defining optimizer options, and
+    % configuring agent-specific options for smooth learning and stability.
+    %
+    % Inputs:
+    %   numObs - Number of observations
+    %   obsInfo - Observation specifications
+    %   numAct - Number of actions
+    %   actInfo - Action specifications
+    %   Ts - Sampling time for the agent
 
-%% Create the actor and critic networks using the createNetworks helper function
-[criticNetwork1,criticNetwork2, ~,actorNetwork] = createNetworks(numObs,numAct);
+    %% Create the actor and critic networks using the helper function
+    [criticNetwork1, criticNetwork2, ~, actorNetwork] = createNetworks(numObs, numAct);
+    % Two critic networks are created to help mitigate overestimation bias in SAC
 
-%% Specify options for the critic and actor representations using rlOptimizerOptions
-criticOptions = rlOptimizerOptions('Optimizer','adam','LearnRate',1e-3,... 
-                                        'GradientThreshold',1);
-actorOptions = rlOptimizerOptions('Optimizer','adam','LearnRate',1e-3,...
-                                       'GradientThreshold',1);
+    %% Specify options for the critic and actor networks with rlOptimizerOptions
+    % Configure optimizer settings for both critic and actor networks
+    criticOptions = rlOptimizerOptions('Optimizer', 'adam', ...
+                                       'LearnRate', 1e-3, ...
+                                       'GradientThreshold', 1);
+    actorOptions = rlOptimizerOptions('Optimizer', 'adam', ...
+                                      'LearnRate', 1e-3, ...
+                                      'GradientThreshold', 1);
 
-%% Create critic and actor representations using specified networks and
-% options
-critic1 = rlQValueFunction(criticNetwork1,obsInfo,actInfo,'ObservationInputNames','observation','ActionInputNames','action');
-critic2 = rlQValueFunction(criticNetwork2,obsInfo,actInfo,'ObservationInputNames','observation','ActionInputNames','action');
-actor  = rlContinuousGaussianActor(actorNetwork,obsInfo,actInfo, ...
-                                   ObservationInputNames="observation", ...
-                                   ActionMeanOutputNames="actionMean", ...
-                                   ActionStandardDeviationOutputNames="actionStd");
+    %% Create critic and actor representations with specified networks and options
+    % Define the Q-value functions (critics) and the Gaussian policy (actor)
+    critic1 = rlQValueFunction(criticNetwork1, obsInfo, actInfo, ...
+                               'ObservationInputNames', 'observation', ...
+                               'ActionInputNames', 'action');
+    critic2 = rlQValueFunction(criticNetwork2, obsInfo, actInfo, ...
+                               'ObservationInputNames', 'observation', ...
+                               'ActionInputNames', 'action');
 
-%% Specify DDPG agent options
-agentOptions = rlSACAgentOptions;
-agentOptions.SampleTime = Ts;
-agentOptions.DiscountFactor = 0.99;
-agentOptions.MiniBatchSize = 256;
-agentOptions.ExperienceBufferLength = 1e6;
-agentOptions.TargetSmoothFactor = 5e-3;
+    % Create the Gaussian actor which outputs mean and standard deviation
+    actor = rlContinuousGaussianActor(actorNetwork, obsInfo, actInfo, ...
+                                      'ObservationInputNames', "observation", ...
+                                      'ActionMeanOutputNames', "actionMean", ...
+                                      'ActionStandardDeviationOutputNames', "actionStd");
 
-agentOptions.NumEpoch = 3;
-agentOptions.MaxMiniBatchPerEpoch = 100;
-agentOptions.LearningFrequency = -1;
-agentOptions.PolicyUpdateFrequency = 1;
-agentOptions.TargetUpdateFrequency = 1;
+    %% Specify SAC agent options for training behavior and performance
+    agentOptions = rlSACAgentOptions;
+    agentOptions.SampleTime = Ts;             % Sampling time step for the agent
+    agentOptions.DiscountFactor = 0.99;       % Discount factor for future rewards
+    agentOptions.MiniBatchSize = 256;         % Batch size for training
+    agentOptions.ExperienceBufferLength = 1e6;% Memory size for experience replay
+    agentOptions.TargetSmoothFactor = 5e-3;   % Target smoothing factor for stability
 
-agentOptions.ActorOptimizerOptions = actorOptions;
-agentOptions.CriticOptimizerOptions = criticOptions;
+    % Set options for the learning process
+    agentOptions.NumEpoch = 3;                % Number of training epochs per update
+    agentOptions.MaxMiniBatchPerEpoch = 100;  % Max mini-batches per epoch
+    agentOptions.LearningFrequency = -1;      % Specifies how often learning occurs
+    agentOptions.PolicyUpdateFrequency = 1;   % Frequency of actor policy updates
+    agentOptions.TargetUpdateFrequency = 1;   % Frequency of target network updates
 
-%% Create agent using specified actor representation, critic representation and agent options
-agent = rlSACAgent(actor,[critic1,critic2],agentOptions);
+    % Attach optimizer options to the agent for actor and critic
+    agentOptions.ActorOptimizerOptions = actorOptions;
+    agentOptions.CriticOptimizerOptions = criticOptions;
+
+    %% Create the SAC agent using the actor and critics along with agent options
+    agent = rlSACAgent(actor, [critic1, critic2], agentOptions);
+
+    % Optional configurations and notes
+    % Uncomment below to explore further customizations
+    % agentOptions.EntropyWeightOptions.EntropyWeight = 0.2; % Adjust entropy weight
+    % agentOptions.EntropyWeightOptions.TargetEntropy = -1.0; % Target entropy for exploration
+
+end
